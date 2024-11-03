53ff1108d8548634007fd9dd84295e8efbfc65d3
amir-ben-shimol <amir.benshi@gmail.com>
2024-11-03 19:57:38 +0200

    fixed friend req issues & compoenents UI

--- apps/mobile/src/app/(autherized)/(home)/index.tsx
+++ apps/mobile/src/app/(autherized)/(home)/index.tsx
@@ -1,48 +1,18 @@
-import { useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
-import { router } from 'expo-router';
-import { NotificationModal } from 'src/components/modals/NotificationModal';
+import { View } from 'react-native';
 import { UIText } from '@/ui/UIText';
 import { useAuth } from '@/lib/providers/authProvider';
-import { useNotificationsStore } from '@/lib/store/useNotificationsStore';
+import { FriendsList } from './components/FriendsList';
 
 const HomePage = () => {
 	const { user } = useAuth();
-	const { userNotifications } = useNotificationsStore();
-	const [showNotificationList, setShowNotificationList] = useState(false);
-
-	const { resetStore } = useNotificationsStore();
-
-	const onStartChat = () => {
-		router.navigate('/playground');
-	};
 
 	return (
-		<View className="flex w-full items-center justify-center">
-			<UIText className="font-RubikBold text-4xl text-blue-900">{`Hello ${user?.firstName}`}</UIText>
+		<View className="flex w-full items-start justify-center">
+			<UIText className="font-RubikBold mt-4 w-full px-2 text-4xl text-blue-900">{`Hello ${user?.firstName}`}</UIText>
 
-			<View className="flex w-full flex-row justify-between px-2 py-4">
-				<TouchableOpacity className="rounded bg-slate-600 px-6 py-2" onPress={onStartChat}>
-					<UIText className="text-lg text-white">Start Chat</UIText>
-				</TouchableOpacity>
+			<View className="flex w-full flex-row justify-between py-4">
+				<FriendsList />
 			</View>
-
-			<TouchableOpacity onPress={resetStore}>
-				<UIText className="text-lg text-red-600">Reset Store</UIText>
-			</TouchableOpacity>
-
-			<TouchableOpacity onPress={() => setShowNotificationList(!showNotificationList)}>
-				<View className="relative">
-					<UIText className="text-lg text-slate-600">🔔</UIText>
-					{userNotifications.length > 0 && (
-						<View className="absolute -right-2 -top-2 rounded-full bg-red-500 px-1">
-							<UIText className="text-xs text-white">{userNotifications.length}</UIText>
-						</View>
-					)}
-				</View>
-			</TouchableOpacity>
-
-			<NotificationModal isVisible={showNotificationList} onHideModal={() => setShowNotificationList(false)} />
 		</View>
 	);
 };
--- packages/types/lib/types/ui/user.ts
+++ packages/types/lib/types/ui/user.ts
@@ -1,10 +1,27 @@
+import type { Notification } from './notification';
+
+export type FriendStatus = 'pending' | 'incoming' | 'declined' | 'approved';
+
+export type Friend = {
+	readonly userId: string;
+	readonly status: FriendStatus;
+};
+
+export type FriendDetails = {
+	readonly status: FriendStatus;
+	readonly userDetails: Friend & Omit<User, 'password' | 'sessionToken' | 'friendsList' | 'notifications'>;
+};
+
 export type User = {
-	_id: string;
-	firstName: string;
-	lastName: string;
-	email: string;
-	country: string;
-	gender: 'male' | 'female' | 'other';
-	age: number;
-	sessionToken?: string;
+	readonly _id: string;
+	readonly firstName: string;
+	readonly lastName: string;
+	readonly email: string;
+	readonly country: string;
+	readonly gender: 'male' | 'female' | 'other';
+	readonly age: number;
+	readonly password: string;
+	readonly sessionToken?: string;
+	readonly friendsList: Friend[];
+	readonly notifications: Notification[];
 };
