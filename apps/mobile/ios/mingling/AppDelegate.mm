a99de50e6b25d37f6f3368ba3e9730cd06e679d7
amir-ben-shimol <amir.benshi@gmail.com>
2024-11-29 11:58:23 +0200

    chore: Update environment variables and clean up unused configurations

--- apps/mobile/ios/mingling/AppDelegate.mm
+++ apps/mobile/ios/mingling/AppDelegate.mm
@@ -1,9 +1,4 @@
 #import "AppDelegate.h"
-// @generated begin react-native-maps-import - expo prebuild (DO NOT MODIFY) sync-f2f83125c99c0d74b42a2612947510c4e08c423a
-#if __has_include(<GoogleMaps/GoogleMaps.h>)
-#import <GoogleMaps/GoogleMaps.h>
-#endif
-// @generated end react-native-maps-import
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
@@ -12,11 +7,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-// @generated begin react-native-maps-init - expo prebuild (DO NOT MODIFY) sync-1ae01fb86680efeb962d1e435d1f66c435acef3a
-#if __has_include(<GoogleMaps/GoogleMaps.h>)
-  [GMSServices provideAPIKey:@"AIzaSyBFm8NOrSpDOPPsBo_vnbg8uUmq3PNbjW0"];
-#endif
-// @generated end react-native-maps-init
   self.moduleName = @"main";
 
   // You can add your custom initial props in the dictionary below.
