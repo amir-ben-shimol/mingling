befb9a69c68828d1e3462c03d422188854029b33
amir-ben-shimol <amir.benshi@gmail.com>
2024-11-30 12:12:06 +0200

    add lint-staged configuration for notifications service

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,6 +2,8 @@ module.exports = {
 	'apps/mobile/**/*.{ts,tsx,cjs}': 'pnpm --filter @mingling/mobile exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
 
 	'apps/backend/**/*.{ts,tsx,cjs}': 'pnpm --filter @mingling/backend exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+	'apps/notifications-service/**/*.{ts,tsx,cjs}':
+		'pnpm --filter @mingling/notifications-service exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
 
 	'packages/types/**/*.ts': 'pnpm --filter @mingling/types" exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
 	'packages/database/**/*.ts': 'pnpm --filter @mingling/database" exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
